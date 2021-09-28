d5aec064bd15aef345809e6b3322c472099eefc3
Ayush Gupta <ayush.keshav2004@gmail.com>
2021-09-28 15:21:47 +0530

    Project Euler - Question 1

---
+++ multiples_of_3_or_5.java
@@ -0,0 +1,13 @@
+
+class multiples_of_3_or_5 
+{
+    public static void main(String[] args) {
+        int sum=0;
+        for(int i=1;i<1000;i++)
+        {
+            if(i%3==0 || i%5==0)
+                sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
