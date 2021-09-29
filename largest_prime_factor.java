ebf930656191cf098b97f9ad653c62eaa48cbdcb
Ayush Gupta <ayush.keshav2004@gmail.com>
2021-09-29 09:54:04 +0530

    Question-3

---
+++ largest_prime_factor.java
@@ -0,0 +1,13 @@
+public class largest_prime_factor {
+    public static void main(String[] args) {
+        long y = (long) 600851475143.0;
+        for(long i = 2; i< y; i++) {
+            while(y%i == 0) {
+               y = y/i;
+            }
+         }
+         if(y >2) {
+            System.out.println(y);
+         }       
+    }
+}
